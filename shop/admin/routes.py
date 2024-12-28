681ccd2ba1cd40b677d03080642a56803cd60216
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-12-28 15:11:46 +0100

    ading the register admin

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -4,3 +4,7 @@ from shop import app,db
 @app.route('/')
 def home():
     return render_template('layout.html',title='Tarek-online')
+
+@app.route('/register')
+def register():
+    return render_template('admin/register.html', title ="Register user")
