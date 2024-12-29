5df1ff4b3a0f74bcbd2b2f9fbbabfdf2a45225b8
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-12-29 12:37:25 +0100

    adding the register form helper

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,10 +1,15 @@
-from flask import render_template , session , redirect , request, url_for
+from flask import render_template , session , redirect , request, url_for,flash
 from shop import app,db
+from.forms import RegistrationForm
 
 @app.route('/')
 def home():
     return render_template('layout.html',title='Tarek-online')
 
-@app.route('/register')
+@app.route('/register',methods = ['GET','POST'])
 def register():
-    return render_template('admin/register.html', title ="Register user")
+    form=RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        flash('you have been registered successfully ')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form, title='REGISTER')
