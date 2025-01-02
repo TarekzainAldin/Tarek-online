2058352ac986955e9d8ad00dc4a2cd58d24d90fe
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-01-02 11:17:48 +0100

    delete the venv

--- shop/admin/models.py
+++ shop/admin/models.py
@@ -1,8 +1,8 @@
 from shop import  db
-
+db = SQLAlchemy()
 class User(db.Model):
    id = db.Column(db.Integer, Primary_Key=True)
-   name=db.Column(db.stering(30), unique=False,nullable=False)
+   name = db.Column(db.String(30), unique=False, nullable=False)
    username=db.Column(db.string(80),unique=False,nullable=False)
    email=db.Column(db.string(120),unique=True,nullable=False)
    password=db.Column(db.string(150),unique=False,nullable=False)
