5bfbc64890b0891f28d3ef58ae5b470be6a1830e
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-05 17:15:59 +0100

    make the order html and recive the product from the cart to the ouder befoure genrate the pdf

--- shop/customer/routes.py
+++ shop/customer/routes.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from flask_login import login_required, current_user, logout_user,login_user
 from shop import app,db, search,bcrypt,login_manger
 from.forms import CustomerRegisterForm,CustomerLoginForm
-from.models import Register
+from.models import CustomerOrder, Register
 import secrets
 import os
 
@@ -41,3 +41,44 @@ def customerLogin():
 def customer_logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+
+@app.route('/getorder')
+@login_required
+def get_order():
+    if current_user.is_authenticated:
+        customer_id = current_user.id
+        invoice = secrets.token_hex(5)
+      
+        try:
+            order = CustomerOrder(invoice=invoice,customer_id=customer_id,orders=session['Shoppingcart'])
+            db.session.add(order)
+            db.session.commit()
+            session.pop('Shoppingcart')
+            flash('Your order has been sent successfully','success')
+            return redirect(url_for('orders',invoice=invoice))
+        except Exception as e:
+            print(e)
+            flash('Some thing went wrong while get order', 'danger')
+            return redirect(url_for('getCart'))
+
+@app.route('/orders/<invoice>')
+@login_required
+def orders(invoice):
+    if current_user.is_authenticated:
+        grandTotal = 0
+        subTotal = 0
+        customer_id = current_user.id
+        customer = Register.query.filter_by(id=customer_id).first()
+        orders = CustomerOrder.query.filter_by(customer_id=customer_id, invoice=invoice).order_by(CustomerOrder.id.desc()).first()
+        for _key, product in orders.orders.items():
+            discount = (product['discount']/100) * float(product['price'])
+            subTotal += float(product['price']) * int(product['quantity'])
+            subTotal -= discount
+            tax = ("%.2f" % (.06 * float(subTotal)))
+            grandTotal = ("%.2f" % (1.06 * float(subTotal)))
+
+    else:
+        return redirect(url_for('customerLogin'))
+    return render_template('customer/order.html', invoice=invoice, tax=tax,subTotal=subTotal,grandTotal=grandTotal,customer=customer,orders=orders)
