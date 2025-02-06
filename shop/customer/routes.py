3be6068d5feda4cd17aa7ba39162afa2f82a3ac8
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-02-06 10:20:14 +0100

    make the genrate the pdf and make the desgin by bootstap

--- shop/customer/routes.py
+++ shop/customer/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,session, request,redirect,url_for,flash,current_app
+from flask import render_template,session, request,redirect,url_for,flash,current_app,make_response
 from flask_login import login_required, current_user
 from flask_login import login_required, current_user, logout_user,login_user
 from shop import app,db, search,bcrypt,login_manger
@@ -6,6 +6,8 @@ from.forms import CustomerRegisterForm,CustomerLoginForm
 from.models import CustomerOrder, Register
 import secrets
 import os
+import json 
+import pdfkit
 
 
 
@@ -82,3 +84,28 @@ def orders(invoice):
     else:
         return redirect(url_for('customerLogin'))
     return render_template('customer/order.html', invoice=invoice, tax=tax,subTotal=subTotal,grandTotal=grandTotal,customer=customer,orders=orders)
+
+@app.route('/get_pdf/<invoice>', methods=['POST'])
+@login_required
+def get_pdf(invoice):
+    if current_user.is_authenticated:
+        grandTotal = 0
+        subTotal = 0
+        customer_id = current_user.id
+        if request.method =="POST":
+            customer = Register.query.filter_by(id=customer_id).first()
+            orders = CustomerOrder.query.filter_by(customer_id=customer_id, invoice=invoice).order_by(CustomerOrder.id.desc()).first()
+            for _key, product in orders.orders.items():
+                discount = (product['discount']/100) * float(product['price'])
+                subTotal += float(product['price']) * int(product['quantity'])
+                subTotal -= discount
+                tax = ("%.2f" % (.06 * float(subTotal)))
+                grandTotal = float("%.2f" % (1.06 * subTotal))
+
+            rendered =  render_template('customer/pdf.html', invoice=invoice, tax=tax,grandTotal=grandTotal,customer=customer,orders=orders)
+            pdf = pdfkit.from_string(rendered, False)
+            response = make_response(pdf)
+            response.headers['content-Type'] ='application/pdf'
+            response.headers['content-Disposition'] ='attached; filename='+invoice+'.pdf'
+            return response
+    return request(url_for('orders'))
